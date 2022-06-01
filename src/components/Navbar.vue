0f045bcdbbf487d7f4493f271db29a325bfff88e
miyayoe <elswazp28@gmail.com>
2022-06-01 19:32:00 +0800

    Complete search bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import axios from 'axios';
+
 const navbarLinks = ref([
   {
     url: '/feed',
@@ -27,6 +29,53 @@ const navbarLinks = ref([
   },
 ]);
 const searchFocus = ref(false);
+const jobArr = ref([]);
+const usersArr = ref([]);
+const articlesArr = ref([]);
+onMounted(() => {
+  (async function () {
+    const jobImgArr = await axios.get(
+      'https://api.unsplash.com/search/photos?query=google-icon&client_id=cZdfd8fqxmmbqYfHGbYbeHxf7nO9WRv4lxG0Wv4_Cc8'
+    );
+    const usersImgArr = await axios.get(
+      'https://api.unsplash.com/search/photos?query=user&client_id=cZdfd8fqxmmbqYfHGbYbeHxf7nO9WRv4lxG0Wv4_Cc8'
+    );
+    const articlesImgArr = await axios.get(
+      'https://api.unsplash.com/search/photos?query=articles&client_id=cZdfd8fqxmmbqYfHGbYbeHxf7nO9WRv4lxG0Wv4_Cc8'
+    );
+    jobArr.value = [
+      {
+        img: jobImgArr.data.results[0].urls.regular,
+        title: 'UX/UI Designer',
+        detail: 'Upwork',
+      },
+      {
+        img: jobImgArr.data.results[1].urls.regular,
+        title: 'Part-time UX designer',
+        detail: 'Google',
+      },
+    ];
+    usersArr.value = [
+      {
+        img: usersImgArr.data.results[0].urls.regular,
+        title: 'Brandon Wilson',
+        detail: 'Senior UX designer',
+      },
+      {
+        img: usersImgArr.data.results[1].urls.regular,
+        title: 'Kyle Fisher',
+        detail: 'Product designer at Commandor Corp',
+      },
+    ];
+    articlesArr.value = [
+      {
+        img: articlesImgArr.data.results[0].urls.regular,
+        title: 'A little about usability testing',
+        detail: '3,912 viewers',
+      },
+    ];
+  })();
+});
 </script>
 
 <template>
@@ -59,47 +108,148 @@ const searchFocus = ref(false);
         <div class="focus-bar">
           <div class="bar-header">
             <input v-focus type="text" placeholder="" />
-            <svg
-              width="40"
-              height="40"
-              viewBox="0 0 40 40"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
+            <a href="javascript:;">
+              <svg
                 width="40"
                 height="40"
-                rx="4"
-                fill="url(#paint0_linear_1_1217)"
-              />
-              <path
-                d="M19.1667 25.8335C22.8486 25.8335 25.8333 22.8487 25.8333 19.1668C25.8333 15.4849 22.8486 12.5001 19.1667 12.5001C15.4848 12.5001 12.5 15.4849 12.5 19.1668C12.5 22.8487 15.4848 25.8335 19.1667 25.8335Z"
-                stroke="white"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M27.5 27.5002L23.875 23.8752"
-                stroke="white"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_1_1217"
-                  x1="20"
-                  y1="0"
-                  x2="20"
-                  y2="40"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="#0077B5" />
-                  <stop offset="1" stop-color="#0E6795" />
-                </linearGradient>
-              </defs>
-            </svg>
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  width="40"
+                  height="40"
+                  rx="4"
+                  fill="url(#paint0_linear_1_1217)"
+                />
+                <path
+                  d="M19.1667 25.8335C22.8486 25.8335 25.8333 22.8487 25.8333 19.1668C25.8333 15.4849 22.8486 12.5001 19.1667 12.5001C15.4848 12.5001 12.5 15.4849 12.5 19.1668C12.5 22.8487 15.4848 25.8335 19.1667 25.8335Z"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M27.5 27.5002L23.875 23.8752"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_1_1217"
+                    x1="20"
+                    y1="0"
+                    x2="20"
+                    y2="40"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#0077B5" />
+                    <stop offset="1" stop-color="#0E6795" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </a>
           </div>
           <hr />
+          <div class="search-hint jobs">
+            <p class="hint-title">JOBS</p>
+            <div v-for="item in jobArr" :key="item.title" class="card">
+              <div class="left">
+                <img :src="item.img" alt="" />
+              </div>
+              <div class="right">
+                <div class="detail">
+                  <p>{{ item.title }}</p>
+                  <span>{{ item.detail }}</span>
+                </div>
+                <svg
+                  width="5"
+                  height="8"
+                  viewBox="0 0 5 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 7.5L4.5 4L1 0.5"
+                    stroke="#CECECE"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div class="hint-footer">
+              <a href=""><p>ALL JOBS (84)</p></a>
+              <hr />
+            </div>
+          </div>
+          <div class="search-hint users">
+            <p class="hint-title">USERS</p>
+            <div v-for="item in usersArr" :key="item.title" class="card">
+              <div class="left">
+                <img :src="item.img" alt="" />
+              </div>
+              <div class="right">
+                <div class="detail">
+                  <p>{{ item.title }}</p>
+                  <span>{{ item.detail }}</span>
+                </div>
+                <svg
+                  width="5"
+                  height="8"
+                  viewBox="0 0 5 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 7.5L4.5 4L1 0.5"
+                    stroke="#CECECE"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div class="hint-footer">
+              <a href=""><p>ALL USERS (1,530)</p></a>
+              <hr />
+            </div>
+          </div>
+          <div class="search-hint articles">
+            <p class="hint-title">ARTICLES</p>
+            <div v-for="item in articlesArr" :key="item.title" class="card">
+              <div class="left">
+                <img :src="item.img" alt="" />
+              </div>
+              <div class="right">
+                <div class="detail">
+                  <p>{{ item.title }}</p>
+                  <span>{{ item.detail }}</span>
+                </div>
+                <svg
+                  width="5"
+                  height="8"
+                  viewBox="0 0 5 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 7.5L4.5 4L1 0.5"
+                    stroke="#CECECE"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div class="hint-footer">
+              <a href=""><p>ALL ARTICLES (30)</p></a>
+              <hr />
+            </div>
+          </div>
+          <div class="all-results">
+            <button><p>ALL RESULTS (2,000+)</p></button>
+          </div>
         </div>
         <div class="focus-filter" @click="searchFocus = false"></div>
       </div>
@@ -224,6 +374,8 @@ const searchFocus = ref(false);
     }
     .focus-bar {
       position: absolute;
+      display: flex;
+      flex-direction: column;
       width: 100%;
       height: 80vh;
       top: 0;
@@ -236,8 +388,91 @@ const searchFocus = ref(false);
         align-items: center;
         justify-content: center;
       }
+      .all-results {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 1rem;
+        background-color: white;
+        margin-top: auto;
+
+        button {
+          width: 100%;
+          border: none;
+          background-color: transparent;
+          background: linear-gradient(180deg, #0077b5 0%, #0e6795 100%);
+          padding: 0.5rem 2.5rem;
+          border-radius: 5px;
+          p {
+            font-size: 1.25rem;
+            transform: scaleY(0.8);
+            color: white;
+          }
+        }
+      }
       hr {
         margin-top: 2rem;
+      }
+      .search-hint {
+        margin: 0 1.5rem;
+        .hint-title {
+          margin: 1rem 0rem;
+          font-size: 1.25rem;
+          color: rgba(24, 24, 24, 0.2);
+        }
+        .hint-footer {
+          p {
+            color: #0275b1;
+            font-weight: bold;
+          }
+        }
+
+        .card {
+          border: 1px solid #f4f4f4;
+          border-radius: 5px;
+          margin: 1rem 0.5rem;
+          padding: 1rem;
+          display: flex;
+          .left {
+            img {
+              width: 2vw;
+              height: 2vw;
+              object-fit: cover;
+              border-radius: 50%;
+            }
+          }
+
+          .right {
+            display: flex;
+            width: 100%;
+            padding-left: 1rem;
+            align-items: center;
+            justify-content: flex-end;
+            .detail {
+              flex-direction: column;
+              justify-content: center;
+              margin-right: auto;
+              p {
+                font-weight: bold;
+              }
+              span {
+                color: #181818;
+              }
+            }
+            svg {
+              width: 15px;
+              height: 15px;
+            }
+          }
+        }
+        &.articles > .card > .left > img {
+          width: 4vw;
+          height: 2.5vw;
+          border-radius: 0%;
+        }
+        hr {
+          margin-top: 1rem;
+        }
       }
       z-index: 100;
     }
